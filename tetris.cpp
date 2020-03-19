3f8fa5773f7ee37b54c24a92db7708e981956e6f
Denilson Sousa <denilsonsousa7@outlook.com>
2020-03-19 12:34:09 -0300

    Logica de colisao feita e mover figura feita

--- tetris.cpp
+++ tetris.cpp
@@ -13,13 +13,14 @@ public:
     int y=100;
     int tam = 10;
     bool colidiu = false;
+    bool isFig = true;
     Quadrado(){}
   	Quadrado(int posX, int posY){
 	  x = posX;
 	  y = posY;
 	}
-    void desenha(){
-    	glColor3f(0.0f,0.0f,1.0f);
+    void desenha(float r, float g, float b){
+    	glColor3f(r,g,b);
         glBegin(GL_QUADS);
                glVertex2i(x-tam,y+tam);
                glVertex2i(x-tam,y-tam);
@@ -39,22 +40,30 @@ public:
     }
     void cair(){
     	if(!colidiu){
-    		printf("TesteQ");
 			y -= tam*2;
 		}
     }
-    void colidir(std::vector<Quadrado*> objsCena){
-    	if((y-tam) <= tam){
-    		printf("Colidiu");
-			colidiu = true;
+    void moverQuad(int direcao){
+			switch(direcao){
+				case 1:
+					x -= tam*2;
+					break;
+				case 2:
+					x += tam*2;
+					break;
+				case 3:
+					y -= tam*2;
+					break;
+			}
 		}
+    void colidir(std::vector<Quadrado*> objsCena){
 		if(objsCena.size() >=1){
 			std::vector<Quadrado*>::iterator it;
 			for(it = objsCena.begin(); it != objsCena.end(); it++){
 				Quadrado *q = (*it);
 				if(q != NULL){
-					if((y-tam) <= (q->y+tam) && x == q->x){
-						printf("Teste");
+					if((y-(tam*2)) <= (q->y+(tam/2)) && x == q->x){
+						printf("Colidiu");
 						colidiu = true;
 					}
 				}
@@ -62,7 +71,7 @@ public:
 		}
     }
     void inGame( std::vector<Quadrado*> objsCena){
-    	desenha();
+    	desenha(0,0,1);
 		colidir(objsCena);
 		cair();
     }
@@ -71,10 +80,11 @@ public:
 //====================================================================== INICIO CLASSE FIGURA ===========================================================
 class Figura{
 	private:
-		 bool colidiu;
+		 
 	public:
 		Quadrado *figura[4];
 		Quadrado *pivot;
+		bool colidiu = false;
 		int myTipe;
 
 		Figura(Quadrado *p, int tipo){
@@ -116,48 +126,62 @@ class Figura{
 			}
 		}
 		
-		void desenharFigura(std::vector<Quadrado*> objsCena){
+		void desenharFigura(){
 			for(int i = 0; i < 4; i ++){
-				figura[i]->desenha();
-				figura[i]->cair();
-				figura[i]->colidir(objsCena);
+				figura[i]->desenha(0,0,1);
 			}
 		}
-		void colidir(){
-			for(int i = 0; i < 4; i++){
-				if(figura[i]->colidiu){
-					colidiu = true;
-				}
-			}
-			if(colidiu){
+		void colidir(std::vector<Quadrado*> objsCena){
+			if(!colidiu){
 				for(int i = 0; i < 4; i++){
-					figura[i]->colidiu = true;
+					figura[i]->colidir(objsCena);
+					if(figura[i]->colidiu){
+						colidiu = true;
+						figura[0]->colidiu = true;
+						figura[1]->colidiu = true;
+						figura[2]->colidiu = true;
+						figura[3]->colidiu = true;
+						
+					}
 				}
-			}
-			
+			}	
 		}
 		void cair(){
 			if(!colidiu){
 				for(int i = 0; i < 4; i++){
-					if(figura[i]->colidiu){
-						figura[i]->cair();
+					figura[i]->cair();	
+				}
+			}		
+		}
+		void moverFig(int direcao){
+			switch(direcao){
+				case 1:
+					for(int i = 0; i < 4; i++){
+						figura[i]->moverQuad(1);
 					}
-				}	
-				
+					break;
+				case 2:
+					for(int i = 0; i < 4; i++){
+						figura[i]->moverQuad(2);
+					}
+					break;
+				case 3:
+					for(int i = 0; i < 4; i++){
+						figura[i]->moverQuad(3);
+					}
+					break;
 			}
-			
 		}
 		void inGame(std::vector<Quadrado*> objsCena){
-			desenharFigura(objsCena);
-			colidir();
+			desenharFigura();
 			cair();
-			
+			colidir(objsCena);	
 		}
 		
 	
 };
 		
-		
+
 
 
 	
@@ -166,10 +190,10 @@ class Figura{
 Quadrado *quad = new Quadrado();
 std::vector<Quadrado*> objsCena;
 std::vector<Quadrado*>::iterator it;
-Figura *fig = new Figura(quad,4);
+Figura *fig = new Figura(quad,3);
 void inicio(void)
 {
-	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 	gluOrtho2D(0,500,0,500);
 }
 void GerenciaTeclado(unsigned char key, int x, int y)
@@ -177,12 +201,12 @@ void GerenciaTeclado(unsigned char key, int x, int y)
     switch (key) {
             case 'd':
                 if(quad->x < 390){
-                    quad->x += 20;
+                    fig->moverFig(2);
                 }
                 break;
             case 'a':
                 if(quad->x > 110){
-                    quad->x -= 20;
+                    fig->moverFig(1);
                 }
                 break;
     }
@@ -204,43 +228,46 @@ void desenhaEixo(){
                glVertex2i(250,500);
         glEnd();
 }
-void desenhaLimites(){
-    glColor3f(0.0f,0.0f,0.0f);
-    glLineWidth(5);
-    glBegin(GL_LINES);
-        glVertex2i(100,500);
-        glVertex2i(100,-500);
-    glEnd();
-
-    glBegin(GL_LINES);
-        glVertex2i(400,500);
-        glVertex2i(400,-500);
-    glEnd();
-    
-    glBegin(GL_LINES);
-        glVertex2i(100,10);
-        glVertex2i(400,10);
-    glEnd();
-
+void verificaColisao(){
+	if(fig->colidiu){
+		for(int i = 0; i < 4; i ++){
+			objsCena.push_back(fig->figura[i]);
+		}
+		fig = new Figura(new Quadrado(250,480),1);
+	}
 }
 void desenhaObjetos(){
 	for(it = objsCena.begin(); it != objsCena.end(); it++){
 		Quadrado *q = (*it);
 		if(q != NULL){
-			q->desenha();
-			q->colidiu=true;
+			if(q->isFig){ // é uma objeto do ttipo figura
+				q->desenha(0,0,1);
+				q->colidiu=true;
+			}else{ // é um objeto do tipo cenario
+				q->desenha(1,1,1);	
+			}
 		}
 	}
 }
 void desenha(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT);
-    //quad->inGame(objsCena);
     fig->inGame(objsCena);
-    //desenhaEixo();
-    desenhaLimites();
-    //desenhaObjetos();
+    desenhaObjetos();
+    verificaColisao();
     glFlush();
+}
+
+void criaCenario(){
+	int x = 90;
+	int y = 20;
+	
+	for(int i =0; i < 15; i++){
+    		Quadrado *quad = new Quadrado(x,y);
+    		quad->isFig = false;
+    		objsCena.push_back(quad);
+    		x+= 20;
+    }
 }
 
 int main(int argc, char **argv)
@@ -253,5 +280,6 @@ int main(int argc, char **argv)
     glutTimerFunc(33,timer,1);
     glutKeyboardFunc(GerenciaTeclado);
     inicio();
+    criaCenario();
     glutMainLoop();
 }
